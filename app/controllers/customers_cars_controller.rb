fb4dc501e2a3530b52c2cf02c2a2012fd21679db
shun k <65330531+lucky-ponta@users.noreply.github.com>
2020-07-20 14:37:42 +0900

    first commit

---
+++ app/controllers/customers_cars_controller.rb
@@ -0,0 +1,2 @@
+class CustomersCarsController < ApplicationController
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_062138) do
+ActiveRecord::Schema.define(version: 2020_07_20_022321) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2020_07_19_062138) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "item_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "customers", force: :cascade do |t|
