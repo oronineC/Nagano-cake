cb21b1cfdcbd2fef49493fd6423bda4d74c15f52
shunsuke2020 <greenpeony9657@gmail.com>
2020-07-25 08:53:12 +0000

    商品情報入力改

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,5 @@
 class Delivery < ApplicationRecord
 	belongs_to :customer
-	has_many :orders, dependent: :destroy
 	validates :address, presence: true
 	validates :postcode, presence: true
 	validates :street_address, presence: true
