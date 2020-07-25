05694cc7e9a64b27e41e04722ed206a6e93fb8af
siragasnow <siraga613@gmail.com>
2020-07-25 22:03:25 +0900

    [update]微調整
    微調整

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
      resources :items, only: [:index, :show]
      resources :carts, only: [:index, :create, :update, :destroy]
 
-  
     delete 'carts' => 'carts#destroy_all'
 
      resources :orders, only:[:new,:create,:index,:show]
