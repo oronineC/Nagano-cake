47e9582bd6521e6cc6e5f23083220901c5d3def7
siragasnow <siraga613@gmail.com>
2020-07-26 14:14:22 +0900

    [add]注文履歴詳細ページ追加
    注文履歴詳細ページ追加

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -6,6 +6,7 @@ class Customers::OrdersController < ApplicationController
 	end
 
 	def show
+		@order = Order.find(params[:id])
 	end
 
 	def new
@@ -23,40 +24,40 @@ class Customers::OrdersController < ApplicationController
 					@order.delivery_post_code = @customer.postcode
 					@order.delivery_street_address = @customer.street_address
 					@order.delivery_address = @customer.current_customer.lastNameKanji+current_customer.first_name_kanji
-					
+
 				when 2
 					@order.delivery_post_code = params[:order][:delivery_post_code]
 					@order.delivery_street_address = params[:order][:delivery_street_address]
 					@order.delivery_address = params[:order][:delivery_address]
-					
+
 				when 3
 					@order.delivery_post_code = params[:order][:delivery_post_code]
 					@order.delivery_street_address = params[:order][:delivery_street_address]
 					@order.delivery_address = params[:order][:delivery_address]
 			end
 			@order.save
-			
+
 			if Delivery.find_by(street_address: @order.delivery_street_address).nil?
 				@delivery = Delivery.new
 				@delivery.postcode = @order.delivery_post_code
 				@delivery.street_address = @order.delivery_street_address
 				@delivery.address = @order.delivery_address
-				@delivery.custmer_id = current_customer.id
+				@delivery.customer_id = current_customer.id
 				@delivery.save
 			end
-			
+
 			current_customer.carts.each do |cart|
 				order_item = @order.order_items.build
 				order_item.order_id = @order.id
-				order_item.item_id = cart.item_id 
+				order_item.item_id = cart.item_id
 				order_item.order_quantity = cart.quantity
 				order_item.order_price = cart.item.price
 				order_item.save
 				cart.destroy
 			end
-			
+
 			redirect_to  customers_orders_thanks_path
-		
+
 		else
 			redirect_to root_path
 		end
@@ -72,27 +73,27 @@ class Customers::OrdersController < ApplicationController
 				@order.delivery_post_code = current_customer.postcode
 				@order.delivery_street_address = current_customer.street_address
 				@order.delivery_address = current_customer.lastNameKanji+current_customer.first_name_kanji
-					
+
 			when 2
 				@oro = params[:order][:delivery_street_address].to_i
 				@delivery_street_address = Delivery.find(@oro)
 				@order.delivery_post_code = @delivery_street_address.postcode
 				@order.delivery_street_address = @delivery_street_address.street_address
 				@order.delivery_address = @delivery_street_address.address
-					
+
 			when 3
-				@order.delivery_post_code = params[:order][:new_add][:delivery_post_code]
-				@order.delivery_street_address = params[:order][:new_add][:delivery_street_address]
-				@order.delivery_address = params[:order][:new_add][:delivery_address]
+				@order.delivery_post_code = params[:order][:new_add][:postcode]
+				@order.delivery_street_address = params[:order][:new_add][:street_address]
+				@order.delivery_address = params[:order][:new_add][:address]
 		end
-		
+
 	end
 
 	def thanks
 	end
-	
+
 	private
 	def order_params
-		params.require(:order).parmit(:created_at, :delivery_street_address, :delivery_address, :delivery_post_code, :method_of_payment, :order_status, :total_fee, order_items_attributes: [:order_id, :item_id, :oreder_quantity, :order_price, :production_status])
+		params.require(:order).permit(:created_at, :delivery_street_address, :delivery_address, :delivery_post_code, :method_of_payment, :order_status, :total_fee, order_items_attributes: [:order_id, :item_id, :oreder_quantity, :order_price, :production_status])
 	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def price_include_tax(price)
     	price = price * 1.1
-    	"#{price.floor}円"
+    	"#{price.floor}"
     end
 end
