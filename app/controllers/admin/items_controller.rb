66c4d7e26e0661e8c4c4ed83c1ba862e59ad4634
siragasnow <siraga613@gmail.com>
2020-07-29 13:53:06 +0900

    [fix]微調整
    微調整

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,8 +1,8 @@
 class Admin::ItemsController < ApplicationController
 	before_action :authenticate_admin!
   def index
-   　@search = Item.ransack(params[:q])
-   　@items = @search.result.page(params[:page])
+    @search = Item.ransack(params[:q])
+    @items = @search.result.page(params[:page])
   end
 
   def show
